cb3b2881de53f6f5f0e43ea346d7c07e387c3722
iqbalhossainmohon <iqbalhossainmohon3057@gmail.com>
2023-05-28 16:10:35 +0600

    category menu bar added

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,9 +1,13 @@
+import { Outlet } from "react-router-dom"
 import NavBar from "../components/Shared/NavBar/NavBar"
 
 const Main = () => {
   return (
     <div>
       <NavBar />
+      <div className="py-28">
+        <Outlet />
+      </div>
     </div>
   )
 }
