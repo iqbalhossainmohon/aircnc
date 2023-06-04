bb79240d84d76c03c23776600b80a4a94522863d
iqbalhossainmohon <iqbalhossainmohon3057@gmail.com>
2023-06-04 17:34:35 +0600

    rooms and category added

--- src/components/Categories/CategoryBox/CategoryBox.jsx
+++ src/components/Categories/CategoryBox/CategoryBox.jsx
@@ -1,8 +1,33 @@
+import queryString from "query-string";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 
-const CategoryBox = ({label, icon: Icon}) => {
+const CategoryBox = ({ label, icon: Icon }) => {
+
+    const [params, setParams] = useSearchParams();
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        let countQuery = {}
+        if (params) {
+            countQuery = queryString.parse(params.toString())
+        }
+        const updateQuery = {
+            ...countQuery, category: label,
+        }
+        const url = queryString.stringifyUrl({
+            url: '/',
+            query: updateQuery,
+        },
+            { skipNull: true }
+        )
+        navigate(url)
+
+    }
+
+
     return (
-        <div className="flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 border-transparent text-neutral-500">
+        <div onClick={handleClick} className="flex flex-col items-center justify-center gap-2 p-3 cursor-pointer border-b-2 hover:text-neutral-800 border-transparent text-neutral-500">
             <Icon size={26} />
             <div className="text-sm font-medium">{label}</div>
         </div>
--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom"
 import NavBar from "../components/Shared/NavBar/NavBar"
+import Footer from "../components/Shared/Footer/Footer"
 
 const Main = () => {
   return (
@@ -8,6 +9,7 @@ const Main = () => {
       <div className="py-28">
         <Outlet />
       </div>
+      <Footer />
     </div>
   )
 }
